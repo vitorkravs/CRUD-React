486e43710e6a54b7f1588d9bc41dd847e3857a34
Vitor Kravszenko <vitorkravs3@gmail.com>
2023-08-26 14:12:51 -0300

    api

---
+++ api/db.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+export const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.PASSWORD_MYSQL,
+    database: 'crud'
+});
